21e7d75b07e915b0bfe0b4944dd4edff789b8fc1
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-04-17 21:30:07 +0600

    registration api done

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const mongodb = require("mongodb");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const mongoUrl = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.paph0zm.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+require("./schema/userSchema");
+
+const User = mongoose.model("UserInfo");
+
+app.post("/register", async (req, res) => {
+  const { email, phone, fullName, password } = req.body;
+  const encryptedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const oldUser = await User.findOne({ email });
+    if (oldUser) {
+      return res.send({ status: false, message: "User already exist" });
+    }
+
+    await User.create({
+      email,
+      phone,
+      fullName,
+      password: encryptedPassword,
+    });
+    res.send({ status: true, message: "User successfully registered" });
+  } catch (error) {
+    res.send({ status: false, message: error.message });
+  }
+});
+
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const oldUser = await User.findOne({ email });
+    if (!oldUser) {
+      return res.send({ status: false, message: "User does\'nt exist." });
+    }
+
+  
+  } catch (error) {
+    res.send({ status: false, message: error.message });
+  }
+});
