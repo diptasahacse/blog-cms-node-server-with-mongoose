41426bd7d5751ac18615059b3877faf5e7e5ffc9
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-04-17 22:41:33 +0600

    implement login functionality

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post("/register", async (req, res) => {
   try {
     const oldUser = await User.findOne({ email });
     if (oldUser) {
-      return res.send({ status: false, message: "User already exist" });
+      return res.send({ success: false, message: "User already exist" });
     }
 
     await User.create({
@@ -52,24 +52,33 @@ app.post("/register", async (req, res) => {
       fullName,
       password: encryptedPassword,
     });
-    res.send({ status: true, message: "User successfully registered" });
+    res.send({ success: true, message: "User successfully registered" });
   } catch (error) {
-    res.send({ status: false, message: error.message });
+    res.send({ success: false, message: error.message });
   }
 });
-
 
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const oldUser = await User.findOne({ email });
-    if (!oldUser) {
-      return res.send({ status: false, message: "User does\'nt exist." });
+    const userExist = await User.findOne({ email });
+    if (!userExist) {
+      return res.send({ success: false, message: "User does not exist." });
     }
 
-  
+    if (await bcrypt.compare(password, userExist.password)) {
+      const token = jwt.sign({}, process.env.JWT_ACCESS_TOKEN_SECRET);
+
+      if (res.status(201)) {
+        return res.json({ success: true, message: "Success", data: token });
+      } else {
+        return res.json({ success: false, message: "Unauthorize" });
+      }
+    }
+
+    return res.json({ success: false, message: "Invalid password" });
   } catch (error) {
-    res.send({ status: false, message: error.message });
+    res.send({ success: false, message: error.message });
   }
 });
